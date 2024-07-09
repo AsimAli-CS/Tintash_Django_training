bef17f026fa95ded51e062e1052c182642bc2787
AsimAli-CS <asimaliii1121@gmai.com>
2024-07-09 07:17:24 -0700

    Django Tutorial 5

--- polls/urls.py
+++ polls/urls.py
@@ -15,7 +15,7 @@ go ahead and add an app_name to set the application namespace:
 """
 app_name = "polls"
 urlpatterns = [
-    path("", views.IndexView.as_view(), name="index"),
+    path("index", views.IndexView.as_view(), name="index"),
     path("<int:pk>/", views.DetailView.as_view(), name="detail"),
     path("<int:pk>/results/", views.ResultsView.as_view(), name="results"),
     path("<int:question_id>/vote/", views.vote, name="vote"),
--- polls/views.py
+++ polls/views.py
@@ -7,14 +7,20 @@ from django.db.models import F
 from django.urls import reverse
 from .models import Choice, Question
 from django.views import generic
+from django.utils import timezone
 
 class IndexView(generic.ListView):
     template_name = "polls/index.html"
     context_object_name = "latest_question_list"
 
     def get_queryset(self):
-        """Return the last five published questions."""
-        return Question.objects.order_by("-pub_date")[:5]
+        """
+        Return the last five published questions (not including those set to be
+        published in the future).
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by("-pub_date")[
+            :5
+        ]
 
 """
 Another way to render
@@ -34,6 +40,11 @@ rendered with the given context.
 class DetailView(generic.DetailView):
     model = Question
     template_name = "polls/detail.html"
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 
 class ResultsView(generic.DetailView):
@@ -61,3 +72,4 @@ def vote(request, question_id):
         # with POST data. This prevents data from being posted twice if a
         # user hits the Back button.
         return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
+ 
