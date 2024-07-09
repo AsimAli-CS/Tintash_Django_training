800bdd5ef9effc110cfdfd1b98be8c0a3344d506
AsimAli-CS <asimaliii1121@gmai.com>
2024-07-09 04:23:47 -0700

    Django tutorial 3

--- polls/urls.py
+++ polls/urls.py
@@ -1,7 +1,24 @@
 from django.urls import path
 
 from . import views
+"""
 
+The tutorial project has just one app, polls. In real Django projects, 
+there might be five, ten, twenty apps or more. How does Django differentiate 
+the URL names between them? For example, the polls app has a detail view, and 
+so might an app on the same project that is for a blog. How does one make it so 
+that Django knows which app view to create for a url when using the {% url %} template tag?
+
+The answer is to add namespaces to your URLconf. In the polls/urls.py file, 
+go ahead and add an app_name to set the application namespace:
+
+"""
+app_name = "polls"
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("index", views.index, name="index"),
+    path("<int:question_id>/", views.detail, name="detail"),
+    # ex: /polls/5/results/
+    path("<int:question_id>/results/", views.results, name="results"),
+    # ex: /polls/5/vote/
+    path("<int:question_id>/vote/", views.vote, name="vote"),
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,46 @@
 from django.http import HttpResponse
-
+from .models import Question
+from django.template import loader
+from django.http import Http404
+from django.shortcuts import render
+def index(request):
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    template = loader.get_template("polls/index.html")
+    context = {
+        "latest_question_list": latest_question_list,
+    }
+    return HttpResponse(template.render(context, request))
+"""
+Another way to render
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    context = {"latest_question_list": latest_question_list}
+    return render(request, "polls/index.html", context)
+
+The render() function takes the request object as its first argument, 
+a template name as its second argument and a dictionary as its optional 
+third argument. It returns an HttpResponse object of the given template 
+rendered with the given context.
+
+"""
+# def index(request):
+#     return HttpResponse("Hello, world. You're at the polls index.")
+
+# def detail(request, question_id):
+#     return HttpResponse("You're looking at question %s." % question_id)
+def detail(request, question_id):
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    # question = get_object_or_404(Question, pk=question_id) - same working as above
+    return render(request, "polls/detail.html", {"question": question})
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
